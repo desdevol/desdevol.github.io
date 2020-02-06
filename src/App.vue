87283b999ce3a3caace7aa25bd73f86d5d6eda43
Lee Yong Wei <ywlee9999@hotmail.com>
2020-02-07 01:48:54 +0800

    first section

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,40 @@
 q-layout(view='hHh lpR fFf')
     //- Header
     Header
+
     //- q-drawer(show-if-above, v-model='left', side='left', bordered)
     //-     // drawer content
     q-page-container
-        Hero
-        //- q-page-scroller(expand, position='top', :scroll-offset='150', :offset='[0, 0]')
-        //-     .col.cursor-pointer.q-pa-sm.bg-accent.text-white.text-center
-        //-         | Scroll back up...
+        q-page
+            Hero
 </template>
 
 <script>
 import Header from './components/Header'
 import Hero from './components/Hero'
+
 export default {
     name: 'LayoutDefault',
+    props: {},
     components: {
         Header,
         Hero
     },
+    mixins: [],
     data() {
         return {
-            leftDrawerOpen: false
+            leftDrawerOpen: false,
+            innerHeight: window.innerHeight
         }
-    }
+    },
+    watch: {},
+    computed: {},
+    created: function () {
+        console.log(this.innerHeight);
+    },
+    beforeDestroy: function () { },
+    mounted: function () { },
+    methods: {},
 }
 </script>
 
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,17 @@
 <template lang="pug">
-    q-page.flex.flex-center(dark)
+.hero.bg-primary.window-height.flex.flex-center
+    .row.justify-center.items-center
         img(
-            alt="Quasar logo"
+            style="max-width: 400px; height: 200px"
             src="../assets/logo.png"
         )
+        .col-xs-12.text-h2.text-white.text-center
+            | Hi, I am
+            span.text-bold.text-accent  Desmond
+        .col-xs-12.text-h4.text-white.text-center
+            | A full-stack
+            span.text-accent  web developer
+            |  from Johor Bahru
 </template>
 
 <style>
@@ -11,6 +19,20 @@
 
 <script>
 export default {
-    name: 'HelloWorld'
+    props: {},
+    components: {},
+    mixins: [],
+    data: function () {
+        return {};
+    },
+    watch: {},
+    computed: {},
+    created: function () { },
+    beforeDestroy: function () { },
+    mounted: function () { },
+    methods: {},
 }
 </script>
+
+<style>
+</style>
