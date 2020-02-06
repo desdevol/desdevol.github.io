b8d21522ad58f1f95156f6723bbad41a7cc707a9
Lee Yong Wei <ywlee9999@hotmail.com>
2020-02-07 01:48:42 +0800

    added logo

--- src/quasar.js
+++ src/quasar.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import 'quasar/dist/quasar.css'
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
-import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-import { Quasar } from 'quasar'
+import "quasar/dist/quasar.css";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import { Quasar } from "quasar";
 
 Vue.use(Quasar, {
     config: {
@@ -22,9 +22,9 @@ Vue.use(Quasar, {
             warning: "#F2C037"
         }
     },
-    components: {
+    components: [
         /* not needed if importStrategy is not 'manual' */
-    },
+    ],
     directives: {
         /* not needed if importStrategy is not 'manual' */
     },
